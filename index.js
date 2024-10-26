e18e7537c540d8ae992fb6674dc5ab4166c1c5f4
Avinashdeshan <avinashdeshan11@gmail.com>
2024-10-26 14:51:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function calc() {
+    let number01 = Number(document.getElementById("txtUserName1").value);
+    let number02 = Number(document.getElementById("txtUserName2").value);
+    let operator = document.getElementById("operator").value;
+    let lblOutput = document.getElementById("lblOutput");
+
+    //let displayText = Number(document.getElementById("displayText").value);
+    //let lblOutput = document.getElementById("lblOutput");
+    //let ans = eval(displayText);
+    //lblOutput.innerHTML = ans;
+    //alert(ans);
+    let ans = 0;
+    if (operator == "+") {
+        ans = number01 + number02;
+    } else if (operator == "-") {
+        ans = number01 - number02;
+    } else if (operator == "*") {
+        ans = number01 * number02;
+    } else {
+        ans = number01 / number02;
+    }
+
+    console.log("Number 01 : " + number01 + "|Number 02 : " + number02 + "|Operator : " + operator);
+    lblOutput.innerHTML = "Number 01 : " + number01 + "|Number 02 : " + number02 + "=" + ans;
+}
